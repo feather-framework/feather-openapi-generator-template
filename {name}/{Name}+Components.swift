d262beb5f9de7f4967ad0cb8e34e9867e51a84ed
GErP83 <gurrka@gmail.com>
2024-01-05 16:39:20 +0100

    add template

---
+++ {name}/{Name}+Components.swift
@@ -0,0 +1,90 @@
+//
+//  {Name}Builder.swift
+//  {project}
+//
+//  Created by {author} on {date}.
+//
+
+import Foundation
+import OpenAPIKit
+import FeatherOpenAPIKit
+
+extension {Name} {
+
+    enum Components {
+
+        // MARK: -
+        
+        static var reference: JSONSchema {
+            .object(
+                properties: [
+                    "id": Generic.Fields.uuid(),
+                    "sampleField": Fields.sampleField(),
+                ]
+            )
+        }
+        
+        // MARK: - list
+
+        static var listItem: JSONSchema {
+            .object(
+                properties: [
+                    "id": Generic.Fields.uuid(),
+                    "sampleField": Fields.sampleField()
+                ]
+            )
+        }
+
+        static var listSortBy: JSONSchema {
+            .enumeration(
+                description: "The sort key for the list",
+                allowedValues: ["sampleField"],
+                defaultValue: "sampleField"
+            )
+        }
+
+        static var listFilterKey: JSONSchema {
+            .enumeration(
+                description: "The sort key for the list",
+                allowedValues:["sampleField"],
+                examples: ["sampleField"]
+            )
+        }
+        
+        // MARK: -
+
+        static var detail: JSONSchema {
+            .object(
+                properties: [
+                    "id": Generic.Fields.uuid(),
+                    "sampleField": Fields.sampleField(),
+                ]
+            )
+        }
+
+        static var create: JSONSchema {
+            .object(
+                properties: [
+                    "sampleField": Fields.sampleField(),
+                ]
+            )
+        }
+
+        static var update: JSONSchema {
+            .object(
+                properties: [
+                    "sampleField": Fields.sampleField(),
+                ]
+            )
+        }
+
+        static var patch: JSONSchema {
+            .object(
+                properties: [
+                    "sampleField": Fields.sampleField(required: false),
+                ]
+            )
+        }
+    }
+
+}
---
+++ {name}/{Name}+Parameters.swift
@@ -0,0 +1,24 @@
+//
+//  {Name}Builder.swift
+//  {project}
+//
+//  Created by {author} on {date}.
+//
+
+import Foundation
+import OpenAPIKit
+import FeatherOpenAPIKit
+
+extension {Name} {
+
+    enum Parameters {
+
+        static var sampleId: OpenAPI.Parameter {
+            .init(
+                name: "sampleId",
+                context: .path,
+                schema: Generic.Fields.uuid()
+            )
+        }
+    }
+}
